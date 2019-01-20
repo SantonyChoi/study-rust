a73107eafa158c6926f36fb34483246c1fa5a350
Seunghoon Choi <santony.choi@gmail.com>
2019-01-21 01:28:25 +0900

    Main

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,35 +1,124 @@
-use rand::Rng;
-use std::cmp::Ordering;
 use std::io;
 
+static UNITS: [&str; 10] = [
+    "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+static TENS: [&str; 10] = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+static MORETENS: [&str; 10] = [
+    "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety",
+];
+
+static LARGEUNITS: [&str; 11] = [
+    "",
+    "",
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion",
+    "sextillion",
+    "septillion",
+    "octillion",
+];
+
 fn main() {
-    println!("Guess the number!");
+    let mut number = String::new();
+    let mut result = String::new();
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Please input number.");
 
-    loop {
-        println!("Please input your guess.");
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
 
-        let mut guess = String::new();
+    let mut number: u64 = match number.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    let mut times: u64 = 0;
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    if number == 0 {
+        println!("zero");
+    } else {
+        loop {
+            times = times + 1;
 
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
+            if number == 0 {
                 break;
             }
+
+            let this_turn: u64 = get_this_turn(&mut number);
+            if this_turn == 0 {
+                continue;
+            }
+
+            if result != String::new() {
+                result = format!(", {}", result);
+            }
+
+            let this_chunk = one_step(this_turn);
+            let this_last_unit = get_last_unit(times as usize);
+
+            result = format!("{} {}{}", this_chunk, this_last_unit, result);
         }
     }
+    println!("{}", result);
+}
+
+fn get_this_turn(number: &mut u64) -> u64 {
+    let this_turn = *number % 1000;
+    *number = *number / 1000;
+    this_turn
+}
+
+fn one_step(number: u64) -> String {
+    let hundred: String = get_hundred((number / 100) as usize);
+    let ten: String = get_ten((number % 100) as usize);
+
+    if number / 100 == 0 {
+        format!("{}", ten)
+    } else if number % 100 == 0 {
+        format!("{}", hundred)
+        format!("{} {}", hundred, ten)
+    }
+}
+
+fn get_hundred(number: usize) -> String {
+    if number == 0 {
+    } else {
+        format!("{} hundred", UNITS[number])
+    }
+}
+
+fn get_ten(number: usize) -> String {
+    if number == 0 {
+        String::new()
+    } else if number < 10 {
+        format!("{}", UNITS[number])
+    } else if number < 20 {
+        format!("{}", TENS[number - 10])
+    } else {
+        let upper = number / 10;
+        let lower = number % 10;
+        format!("{} {}", MORETENS[upper], UNITS[lower])
+    }
+}
+
+fn get_last_unit(number: usize) -> &'static str {
+    LARGEUNITS[number]
 }
